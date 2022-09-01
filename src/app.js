a9b8698281a86b28f0e89ff606d2cc2692cf9b04
Shreyas Pangal <shreyaspangal03@gmail.com>
2022-09-01 23:48:54 +0530

    path update

--- src/app.js
+++ src/app.js
@@ -161,7 +161,7 @@ function generateBankStates(bankName) {
 
     console.log(`${getFileName}.json`);
 
-    const data = fs.readFileSync(`app/src/db/${getFileName}.json`, 'utf-8');
+    const data = fs.readFileSync(`/app/src/db/${getFileName}.json`, 'utf-8');
 
     const bankObject = JSON.parse(data);
     const bankEntries = Object.entries(bankObject);
@@ -174,13 +174,13 @@ function generateBankStates(bankName) {
         return bankStatesRaw.indexOf(item) === index; // returns unique values
     })
 
-    fs.writeFileSync('app/src/data/BANK_STATES.json', JSON.stringify(bankStates));
+    fs.writeFileSync('/app/src/data/BANK_STATES.json', JSON.stringify(bankStates));
 
     // console.log('Generated Bank State Names');
 }
 
 function getBankStates() {
-    const data = fs.readFileSync('app/src/data/BANK_STATES.json', 'utf-8');
+    const data = fs.readFileSync('/app/src/data/BANK_STATES.json', 'utf-8');
     // console.log('Read Bank State Names');
     return data;
 }
