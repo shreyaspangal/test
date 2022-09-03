caa6c374e8d418ecad257f4f01998b56884baa5a
Shreyas Pangal <shreyaspangal03@gmail.com>
2022-09-03 20:46:56 +0530

    Remove mongodb connection

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const app = require("./app");
 const config = require("./config/config");
 const PORT = config.port;
 
-mongoose
-  .connect(config.mongoose.url, config.mongoose.options)
-  .then(() => console.log("=> Connected to DB"))
-  .catch(error => console.error(error));
+// mongoose
+//   .connect(config.mongoose.url, config.mongoose.options)
+//   .then(() => console.log("=> Connected to DB"))
+//   .catch(error => console.error(error));
 
 app.listen(PORT, () =>
   console.log(`=> Listening to port ${PORT}`)
